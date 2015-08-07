41ecf4e39480ff82102a3906d4b33cbfb52a676a
Puck Meerburg <puck@puckipedia.nl>
2015-08-07 14:59:51 +0200

    Add rando

---
+++ CardsAgainstIRC3/Game/Bots/Rando.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsAgainstIRC3.Game.Bots
+{
+    [Bot("rando")]
+    public class Rando : IBot
+    {
+        public GameManager Manager
+        {
+            get;
+            private set;
+        }
+
+        public Rando(GameManager manager)
+        {
+            Manager = manager;
+        }
+
+        public Card[] ResponseToCard(Card blackCard)
+        {
+            return Manager.TakeWhiteCards(blackCard.Parts.Length - 1).ToArray();
+        }
+    }
+}
